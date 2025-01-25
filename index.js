8d7d9db1eda5f310aa74ed8e8151e75273bc2498
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-01-25 13:32:22 -0600

    Add functionality to generate passwords button. Generated passwords are copied when clicked.

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 
-    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-    "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
-
-const digits = "0123456789";
+const numbers = "0123456789";
 const asciiLowercase = "abcdefghijklmnopqrstuvwxyz";
 const asciiUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const punctuation = `!"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~`;
+const specialCharacters = `!"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~`;
 
 const passwordLengthValue = document.querySelector("#value");
 const passwordLengthEl = document.querySelector("#password-length");
@@ -15,4 +10,86 @@ passwordLengthEl.addEventListener("input", (event) => {
     passwordLengthValue.textContent = event.target.value;
 });
 
-// console.log(input.value)
+let hasUpper = false;
+let hasNumber = false;
+let hasSpecial = false;
+
+const upperEl = document.querySelector("#upper");
+upperEl.addEventListener("change", function() {
+    hasUpper = this.checked;
+});
+
+const numberEl = document.querySelector("#numbers") 
+numberEl.addEventListener("change", function() {
+    hasNumber = this.checked;
+});
+
+const specialEl = document.querySelector("#special")
+specialEl.addEventListener("change", function() {
+    hasSpecial = this.checked;
+});
+
+function binary() {
+    return Math.floor(Math.random() * 2);
+};
+
+function randomCharacter(characterSet) {
+    return Math.floor(Math.random() * characterSet.length);
+};
+
+function addCharacter(password, characterSet) {
+    let randomIndex = randomCharacter(characterSet);
+    return binary() === 0 ? password + characterSet[randomIndex] : characterSet[randomIndex] + password;
+};
+
+passwordOneEl = document.querySelector("#passwordOne");
+passwordTwoEl = document.querySelector("#passwordTwo");
+
+function generatePasswords() {
+    passwordOneEl.textContent = generatePassword();
+    passwordTwoEl.textContent = generatePassword();
+};
+
+function generatePassword() {
+    let characterSet = asciiLowercase;
+    let password = "";
+    password = addCharacter(password, characterSet);
+    if (hasUpper) {
+        password = addCharacter(password, asciiUppercase);
+        characterSet += asciiUppercase;
+    };
+    if (hasNumber) {
+        password = addCharacter(password, numbers);
+        characterSet += numbers;
+    };
+    if (hasSpecial) {
+        password = addCharacter(password, specialCharacters);
+        characterSet += specialCharacters;
+    };
+    for (let i = password.length; i < passwordLengthEl.value; i++) {
+        password = addCharacter(password, characterSet);
+    };
+    return password;
+};
+
+function copyPassword(passwordEl) {
+    let passwordButton = document.querySelector(`#${passwordEl}`);
+    copyText = passwordButton.textContent;
+    navigator.clipboard.writeText(copyText);
+    alert("Copied password!");
+};
+
+function generateOnePasswordTest() {
+    let password = "";
+    let cset = numbers + asciiLowercase + asciiUppercase + specialCharacters;
+    for (let i = 0; i < 10; i++) {
+        password = addCharacter(password, cset);
+    };
+    return password;
+};
+
+function generateMultiplePasswords(number) {
+    for (let i = 0; i < number; i++) {
+        console.log(generateOnePasswordTest());
+    };
+};
